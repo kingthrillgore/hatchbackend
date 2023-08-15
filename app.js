3c63070e18795ac6b34bbe2c3aa97f3803dd9f09
Cameron Kilgore <ghostfreeman@gmail.com>
2023-08-15 18:46:37 -0500

    Switched app to store everything in memory for now

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 const datefns = require('date-fns')
 const express = require('express')
 const cors = require('cors')
+const bodyParser = require('body-parser')
 const router = express.Router()
 //const database = require('./database')
 //const Party = require('./models/party')
 
-const party = []
+let parties = []
 const port = 8080
 
 const app = express()
@@ -25,27 +26,36 @@ app.post('/reserve', async(req, res) => {
         res.status(400).send()
     }
 
-    const party = new Party(req.body)
+    console.log("req", req.body)
+    console.log("name", req.body.name)
+    console.log("size", req.body.size)
+
+    const party_request = {
+        'name': req.body.name,
+        'size': req.body.size
+    }
+
+    parties.push(party_request);
 
     try {
         party.save()
-        res.send(party)
+        res.send(req)
     } catch (error) {
         res.status(500).send(error)
     }
 })
 
 app.get("/reserve", async (req, res) => {
-    const party = await Party.find({})
+    //const party = await Party.find({})
+
+    console.log("Parties", parties)
 
     try {
-        res.send(party)
+        res.send(parties)
     } catch (error) {
         res.status(500).send(error)
     }
 });
-
-// TODO add for specific ID call?
 
 app.listen(port, function () {
     console.log('App listening on port ' + port + '!')
