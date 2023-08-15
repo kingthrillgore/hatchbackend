c603b3e07936bbaa443030676d1a4fad9eb3417e
Cameron Kilgore <ghostfreeman@gmail.com>
2023-08-15 16:59:43 -0500

    Added initial endpoints

---
+++ app.js
@@ -0,0 +1,35 @@
+const datefns = require('date-fns')
+const express = require('express')
+const cors = require('cors')
+const router = express.Router()
+const database = require('./database')
+const Party = require('./models/party')
+
+const port = 8080
+
+const app = express()
+app.use(express.json())
+app.use(cors())
+
+app.get('/', (req, res) => {
+    res.json({'greeting': 'Hello World!'})
+})
+
+app.get('/healthcheck', (req, res) => {
+    res.json({"heartbeat": true})
+})
+
+app.post('/reserve', async(req, res)) {
+    if (!req.body) {
+        res.status(400).send()
+    }
+
+    const party = new Party(req.body)
+
+    try {
+        party.save()
+        res.send(party)
+    } catch (error) {
+        res.status(500).send(error)
+    }
+}
